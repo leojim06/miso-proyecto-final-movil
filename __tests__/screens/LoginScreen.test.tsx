dcf273a5805b9861866789a89fd68860160b44cd
o.jimenez-at-285254948890 <leojim06@gmail.com>
2022-10-22 23:29:38 -0500

    agregar test

---
+++ __tests__/screens/LoginScreen.test.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import renderer from "react-test-renderer"
+
+import LoginScreen from "../../screens/LoginScreen"
+
+describe("<LoginScreen />", () => {
+
+  it("renders without crashing", () => {
+    shallow(<LoginScreen />);
+  });
+
+  it("has 2 child", () => {
+    const tree: any = renderer.create(<LoginScreen />).toJSON()
+    expect(tree?.children?.length).toBe(2)
+  })
+
+//   it('renders correctly', () => {
+//     const tree = renderer.create(<AppTest />).toJSON();
+//     expect(tree).toMatchSnapshot();
+//   });
+})
