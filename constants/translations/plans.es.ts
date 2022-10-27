36f93326f11724eb6fca9d002762634e6adf872a
o.jimenez-at-285254948890 <leojim06@gmail.com>
2022-10-26 20:48:30 -0500

    adicion de configuracion para traduccion al español

---
+++ constants/translations/plans.es.ts
@@ -0,0 +1,13 @@
+export const plansEs = {
+    label: {
+        title: 'Planes',
+        myPlans: 'Mis Planes',
+        suggestedPlans: 'Planes sugeridos'
+    },
+    trainigPlan: {
+        label: {
+            level: 'Nivel: ',
+            duration: 'Duración: ',
+        }
+    }
+}
